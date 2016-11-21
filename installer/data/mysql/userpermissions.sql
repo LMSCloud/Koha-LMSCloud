4bdc83346e18cd8c5138ad6fa3053895568d4d23
Roger Großmann <roger.grossmann@lmscloud.de>
2016-11-21 16:32:23 +0100

    add new permission 'edit_notice_fee_rules' to inital permission set

--- installer/data/mysql/userpermissions.sql
+++ installer/data/mysql/userpermissions.sql
@@ -57,6 +57,7 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    (13, 'records_batchmod', 'Perform batch modification of records (biblios or authorities)'),
    (13, 'marc_modification_templates', 'Manage marc modification templates'),
    (13, 'records_batchdel', 'Perform batch deletion of records (bibliographic or authority)'),
+   (13, 'edit_notice_fee_rules', 'Define notice fee rules'),
    (15, 'check_expiration', 'Check the expiration of a serial'),
    (15, 'claim_serials', 'Claim missing serials'),
    (15, 'create_subscription', 'Create a new subscription'),
