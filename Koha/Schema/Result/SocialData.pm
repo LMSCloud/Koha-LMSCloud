7ff7886e561acf4df7fa8c1d63183613ba13f0d4
Roger Großmann <roger.grossmann@lmscloud.de>
2017-11-13 13:50:25 +0100

    Schema changes

--- Koha/Schema/Result/SocialData.pm
+++ Koha/Schema/Result/SocialData.pm
@@ -26,7 +26,6 @@ __PACKAGE__->table("social_data");
 =head2 isbn
 
   data_type: 'varchar'
-  default_value: (empty string)
   is_nullable: 0
   size: 30
 
@@ -65,7 +64,7 @@ __PACKAGE__->table("social_data");
 
 __PACKAGE__->add_columns(
   "isbn",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 30 },
+  { data_type => "varchar", is_nullable => 0, size => 30 },
   "num_critics",
   { data_type => "integer", is_nullable => 1 },
   "num_critics_pro",
@@ -93,8 +92,8 @@ __PACKAGE__->add_columns(
 __PACKAGE__->set_primary_key("isbn");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2017-09-07 12:38:41
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:KGiWtjcsWury2tvo0QwdMg
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2017-11-10 17:46:37
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:dSh7Dtm42nhfKByZs+nRYQ
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
