312d68751bd6bf6bd7a65c23354e8298d412d83a
Roger Großmann <roger.grossmann@lmscloud.de>
2020-04-14 14:57:28 +0200

    Take over calender copy function from the Koha head revision. Add an option to copy only unique holidays and date exceptions of a selected date range.

--- tools/copy-holidays.pl
+++ tools/copy-holidays.pl
@@ -23,22 +23,42 @@ use CGI qw ( -utf8 );
 
 use C4::Auth;
 use C4::Output;
-
-
 use C4::Calendar;
+
+use Koha::DateUtils;
 
 my $input               = new CGI;
 my $dbh                 = C4::Context->dbh();
 
+checkauth($input, 0, {tools=> 'edit_calendar'}, 'intranet');
+
 my $branchcode          = $input->param('branchcode');
 my $branchgroup         = $input->param('branchgroup');
 my $from_branchcode     = $input->param('from_branchcode');
+my $limitcopydaterange  = $input->param('limitcopydaterange');
+my $datefrom            = $input->param('datefrom');
+my $dateto              = $input->param('dateto');
 
-if ( $branchgroup && $from_branchcode ) {
-    C4::Calendar->new(branchcode => $from_branchcode)->copy_to_group($branchgroup);
+if ( $limitcopydaterange ) {
+    $datefrom = eval { output_pref({ dt => dt_from_string( $datefrom ), dateformat => 'iso', dateonly => 1 } ); };
+    $dateto = eval { output_pref({ dt => dt_from_string( $dateto ), dateformat => 'iso', dateonly => 1 } ); };
+    
+    if ( $datefrom && $dateto ) {
+        if ( $branchgroup && $from_branchcode ) {
+            C4::Calendar->new(branchcode => $from_branchcode)->copy_to_group_special($branchgroup,$datefrom,$dateto);
+        }
+        elsif ( $branchcode && $from_branchcode  ) {
+            C4::Calendar->new(branchcode => $from_branchcode)->copy_to_branch_special($branchcode,$datefrom,$dateto);
+        }
+    }
 }
-elsif ( $branchcode && $from_branchcode  ) {
-    C4::Calendar->new(branchcode => $from_branchcode)->copy_to_branch($branchcode);
+else {
+    if ( $branchgroup && $from_branchcode ) {
+        C4::Calendar->new(branchcode => $from_branchcode)->copy_to_group($branchgroup);
+    }
+    elsif ( $branchcode && $from_branchcode  ) {
+        C4::Calendar->new(branchcode => $from_branchcode)->copy_to_branch($branchcode);
+    }
 }
 
 print $input->redirect("/cgi-bin/koha/tools/holidays.pl?branch=".($branchcode || $from_branchcode));
