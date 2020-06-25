338264f5166ed8bb3b3bc36dda921b162dc1aff9
Roger Großmann <roger.grossmann@lmscloud.de>
2020-06-25 14:41:58 +0200

    Make links to social networks in OPAC detail page configurable.

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -17194,6 +17194,22 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+
+$DBversion = '18.05.05.015';
+if( CheckVersion( $DBversion ) ) {
+    my ($socialnetworks) = $dbh->selectrow_array( q|
+        SELECT value FROM systempreferences WHERE variable='socialnetworks';
+    |);
+    if( $socialnetworks ){
+        # If the socialnetworks preference is enabled, enable all social networks
+        $dbh->do("UPDATE systempreferences SET value = 'email,facebook,linkedin,twitter', options = 'email|facebook|linkedin|twitter', type = 'multiple'  WHERE variable = 'SocialNetworks'");
+    } else {
+        $dbh->do("UPDATE systempreferences SET value = '', options = 'email|facebook|linkedin|twitter', type = 'multiple'  WHERE variable = 'SocialNetworks'");
+    }
+    SetVersion ($DBversion);
+    print "Upgrade to $DBversion done (Bug 22880: Allow granular control of socialnetworks preference)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
