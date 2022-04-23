88ddf4884447a292c16c172d906f51225d8e622a
Roger Großmann <roger.grossmann@lmscloud.de>
2022-04-23 18:58:25 +0200

    Fix error indexing authorities.

--- Koha/SearchEngine/Elasticsearch.pm
+++ Koha/SearchEngine/Elasticsearch.pm
@@ -593,8 +593,10 @@ sub marc_records_to_documents {
             if( $authtypecode ){
                 if( $authtypecode !~ m/_SUBD/ ){ #Subdivision records will not be used for linking and so don't require match-heading to be built
                     my $field = $record->field( $auth_match_headings{ $authtypecode } );
-                    my $heading = C4::Heading->new_from_field( $field, undef, 1 ); #new auth heading
-                    push @{$record_document->{'match-heading'}}, $heading->search_form if $heading;
+                    if ( $field ) {
+                        my $heading = C4::Heading->new_from_field( $field, undef, 1 ); #new auth heading
+                        push @{$record_document->{'match-heading'}}, $heading->search_form if $heading;
+                    }
                 }
             } else {
                 warn "Cannot determine authority type for record: " . $record->field('001')->as_string;
