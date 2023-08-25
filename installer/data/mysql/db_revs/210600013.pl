2032658f187ee3c2ada34322f7f301d4b82cf009
Roger Grossmann <roger.grossmann@lmscloud.de>
2023-08-25 15:50:32 +0200

    The consolidation of the field content of account_offsets.type lacks the correct handling of the formerly reversed payments. Without a special handling, it would not be possible to identify reversed payments in accountline offsets. Introducing value 'REVERSED' as account_offsets.type makes it possible to create correct fines reports for past periods with reversed payments.

--- installer/data/mysql/db_revs/210600013.pl
+++ installer/data/mysql/db_revs/210600013.pl
@@ -16,11 +16,12 @@ return {
         $dbh->do( "DROP TABLE IF EXISTS account_offset_types" );
 
         # Update offset_types to 'CREATE' where appropriate
-        $dbh->do( "UPDATE account_offsets SET type = 'CREATE' WHERE type != 'OVERDUE_INCREASE' AND type != 'OVERDUE_DECREASE' AND ( debit_id IS NULL OR credit_id IS NULL)" );
+        $dbh->do( "UPDATE account_offsets SET type = 'CREATE' WHERE type != 'OVERDUE_INCREASE' AND type != 'OVERDUE_DECREASE' AND type != 'Reverse Payment' AND ( debit_id IS NULL OR credit_id IS NULL)" );
         $dbh->do( "UPDATE account_offsets SET amount = ABS(amount) WHERE type = 'CREATE'" );
+        $dbh->do( "UPDATE account_offsets SET type = 'REVERSED' WHERE type = 'Reverse Payment'" );
 
         # Update offset_types to 'APPLY' where appropriate
-        $dbh->do( "UPDATE account_offsets SET type = 'APPLY' WHERE type != 'OVERDUE_INCREASE' AND type != 'OVERDUE_DECREASE' AND type != 'CREATE' AND type != 'VOID'" );
+        $dbh->do( "UPDATE account_offsets SET type = 'APPLY' WHERE type != 'OVERDUE_INCREASE' AND type != 'OVERDUE_DECREASE' AND type != 'CREATE' AND type != 'VOID' AND type != 'REVERSED'" );
 
         # Update table to ENUM
         $dbh->do(
@@ -33,7 +34,8 @@ return {
                         'APPLY',
                         'VOID',
                         'OVERDUE_INCREASE',
-                        'OVERDUE_DECREASE'
+                        'OVERDUE_DECREASE',
+                        'REVERSED'
                     ) NOT NULL
                 AFTER `debit_id`
               }
