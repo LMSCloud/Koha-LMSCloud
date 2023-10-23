505560dbcd06dc1afc99c71b6d0d528d31012a29
LMSCloudFranzW <franz@oha04>
2023-10-23 13:59:16 +0200

    Adaptions for version 22.11.

--- Koha/REST/V1/Illrequests.pm
+++ Koha/REST/V1/Illrequests.pm
@@ -44,8 +44,42 @@ sub list {
     my $c = shift->openapi->valid_input or return;
 
     return try {
+        my $backendsStringMap = {};    # mapping of designation used in a specific backend's illrequestattributes.type to designation assumed in the general ILL framework
+        my $config = Koha::Illrequest::Config->new;
+        my $backends = $config->available_backends;
+
+        foreach my $b (@$backends) {
+            my $backend = Koha::Illrequest->new->load_backend($b);
+
+            my $backendStringMap = $backend->_backend_capability( 'getStringMap', $backend );
+            if ( ! $backendStringMap ) {
+                $backendStringMap = {};
+            }
+            $backendsStringMap->{$b} = $backendStringMap;
+        }
+
 
         my $reqs = $c->objects->search(Koha::Illrequests->new->filter_by_visible);
+
+        # Via backendsStringMap we rename the specific backend's illrequestattributes.type to the type designation used in the general ILL framework (e.g. in in ill-list-table.js).
+        foreach my $req ( @{$reqs} ) {
+            my $ill_backend_id = $req->{ill_backend_id};
+            if ( $backendsStringMap->{$ill_backend_id} && $backendsStringMap->{$ill_backend_id}->{attrType} ) {
+                foreach my $frameworkAttrType ( keys %{$backendsStringMap->{$ill_backend_id}->{attrType}} ) {
+                    my $backendAttrType = $frameworkAttrType;
+                    $backendAttrType = $backendsStringMap->{$ill_backend_id}->{attrType}->{$frameworkAttrType};
+                    if ( $backendAttrType && ($backendAttrType ne $frameworkAttrType) ) {
+                        foreach my $illattr ( @{$req->{extended_attributes}} ) {
+                            my $type = $illattr->{type};
+                            if ( $type && $type eq $backendAttrType ) {
+                                $illattr->{type} = $frameworkAttrType;
+                                last;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         return $c->render(
             status  => 200,
