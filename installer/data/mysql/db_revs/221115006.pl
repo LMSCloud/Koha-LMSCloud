1eacdbc8dfee769c64d8f98d241af569ebdc0904
roger <roger@kallarm.fritz.box>
2024-08-13 13:52:02 +0200

    Set system parameter defaults for LMSCloud Koha.

---
+++ installer/data/mysql/db_revs/221115006.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+
+return {
+    bug_number => "",
+    description => "UPDATE default system preferences to LMSCloud Koha standard.",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh) = @$args{qw(dbh)};
+        
+        $dbh->do(q{UPDATE systempreferences set value = '0' WHERE variable = 'TranslateNotices'});
+        $dbh->do(q{UPDATE systempreferences set value = '1' WHERE variable = 'showLastPatron'});
+        $dbh->do(q{UPDATE systempreferences set value = '50' WHERE variable = 'numReturnedItemsToShow'});
+        $dbh->do(q{UPDATE systempreferences set value = '1' WHERE variable = 'IntranetAddMastheadLibraryPulldown'});
+        $dbh->do(q{UPDATE systempreferences set value = 'starts_with' WHERE variable = 'DefaultPatronSearchMethod'});
+        $dbh->do(q{UPDATE systempreferences set value = '1' WHERE variable = 'OPACSuggestionAutoFill'});
+        $dbh->do(q{UPDATE systempreferences set value = 'transfer' WHERE variable = 'ListOwnershipUponPatronDeletion'});
+        $dbh->do(q{UPDATE systempreferences set value = 'codemirror' WHERE variable = 'AdditionalContentsEditor'});
+    },
+};
