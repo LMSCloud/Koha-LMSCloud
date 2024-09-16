674222233953a83c85cec15ce3846d62707b0edf
Paul Derscheid <paul.derscheid@lmscloud.de>
2024-09-16 10:06:38 +0000

    Bug 37592: (QA follow-up) Change created_on, updated_on to creation_date, modification_date
    It makes sense not to introduce mapping code if there's no reason for it.
    Accordingly the the columns are now of type DATETIME instead.
    
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit e75c1ac012b8f12322e713f45f94e0054a84f58d)

--- Koha/Booking.pm
+++ Koha/Booking.pm
@@ -236,10 +236,7 @@ on the API.
 =cut
 
 sub to_api_mapping {
-    return {
-        created_on => "creation_date",
-        updated_on => "modification_date"
-    };
+    return {};
 }
 
 =head2 Internal methods
