PTFS-Europe/koha

Bug 34440: Take preparation period into account at checkout
This patch ensures we catch checkouts that would interfer with the
preparation period set for an existing booking by adding said period to
the bookings search.

We also reduce the proposed due date for the checkout to be confirmed by
the preparation period for the item.

Test plan
1) Set a booking preparation period for an itemtype
2) Add a booking for an item with that itemtype to fall beyond the due
   date that the item would be assigned if you were to check it out now
   to your test user, but within the number of days you've set for your
   preparation period.
3) Attempt to check the item out to your test user.
4) Note that we catch the booking and warn and that the latest return
   date suggested take the preparation period into account.

circ/circulation.pl
@@ -51,6 +51,7 @@ use Koha::Plugins;
 use Koha::Database;
 use Koha::BiblioFrameworks;
 use Koha::Items;
+use Koha::CirculationRules;
 use Koha::SearchEngine;
 use Koha::SearchEngine::Search;
 use Koha::Patron::Modifications;
@@ -444,8 +445,17 @@ if (@$barcodes && $op eq 'cud-checkout') {
                 $template_params->{NEEDSCONFIRMATION} = 1;
                 $confirm_required = 1;
                 if ( $needsconfirmation_key eq 'BOOKED_TO_ANOTHER' ) {
+                    my $rule = Koha::CirculationRules->get_effective_rule(
+                        {
+                            rule_name  => 'bookings_lead_period',
+                            itemtype   => $item->effective_itemtype,
+                            branchcode => "*"
+                        }
+                    );
+                    my $preparation_period = $rule ? $rule->rule_value : 1;
                     my $reduceddue =
-                        dt_from_string( $$needsconfirmation{$needsconfirmation_key}->start_date )->subtract( days => 1 );
+                        dt_from_string( $$needsconfirmation{$needsconfirmation_key}->start_date )
+                        ->subtract( days => $preparation_period );
                     $template_params->{reduceddue} = $reduceddue;
                 }
             }

LMSCloud/Koha-LMSCloud

Sponsored-by: Cuyahoga County Public Library <https://cuyahogalibrary.org/>
Signed-off-by: Kristi Krueger <KKRUEGER@cuyahogalibrary.org>
Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
(cherry picked from commit 620527f97140370eedc6c2fb35930f770b97ffb3)

circ/circulation.pl
@@ -53,6 +53,7 @@ use Koha::Plugins;
 use Koha::Database;
 use Koha::BiblioFrameworks;
 use Koha::Items;
+use Koha::CirculationRules;
 use Koha::SearchEngine;
 use Koha::SearchEngine::Search;
 use Koha::Patron::Modifications;
@@ -432,8 +433,17 @@ if (@$barcodes) {
                 $template_params->{NEEDSCONFIRMATION} = 1;
                 $confirm_required = 1;
                 if ( $needsconfirmation eq 'BOOKED_TO_ANOTHER' ) {
+                    my $rule = Koha::CirculationRules->get_effective_rule(
+                        {
+                            rule_name  => 'bookings_lead_period',
+                            itemtype   => $item->effective_itemtype,
+                            branchcode => "*"
+                        }
+                    );
+                    my $preparation_period = $rule ? $rule->rule_value : 1;
                     my $reduceddue =
-                        dt_from_string( $$question{$needsconfirmation}->start_date )->subtract( days => 1 );
+                        dt_from_string( $$question{$needsconfirmation}->start_date )
+                        ->subtract( days => $preparation_period );
                     $template_params->{reduceddue} = $reduceddue;
                 }
             }