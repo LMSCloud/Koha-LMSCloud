2e50ba16a1811ebd13c29dc0451a3588da9388a5
Paul Derscheid <paul.derscheid@lmscloud.de>
2025-07-08 17:55:50 +0200

    tmp(fix): prevent empty vis-timeline entries when updating booking
    - Always use store.bookingPatron instead of the API response field in bookingUtils.mjs.
        - It is always available through the main initialization logic in
          BookingModal.vue.
    - Clean up the messy nested ternaries in favor of a helper method.
    - This patch needs to be upstreamed to the implementation based on main.

--- koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingUtils.mjs
+++ koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingUtils.mjs
@@ -8,6 +8,21 @@ export function debounce(fn, delay) {
     };
 }
 
+function renderContent(bookingPatron) {
+    if (typeof window.$patron_to_html === "function" && bookingPatron) {
+        return window.$patron_to_html(bookingPatron, {
+            display_cardnumber: true,
+            url: true,
+        });
+    }
+
+    if (bookingPatron?.cardnumber) {
+        return bookingPatron.cardnumber;
+    }
+
+    return "";
+}
+
 export function updateExternalDependents(store, newBooking, isUpdate = false) {
     if (typeof window.timeline !== "undefined" && window.timeline !== null) {
         const itemData = {
@@ -16,21 +31,7 @@ export function updateExternalDependents(store, newBooking, isUpdate = false) {
             patron: newBooking.patron_id,
             start: dayjs(newBooking.start_date).toDate(),
             end: dayjs(newBooking.end_date).toDate(),
-            content:
-                typeof window.$patron_to_html === "function"
-                    ? window.$patron_to_html(
-                          !isUpdate && store.bookingPatron
-                              ? store.bookingPatron
-                              : newBooking.patron,
-                          { display_cardnumber: true, url: false }
-                      )
-                    : !isUpdate &&
-                        store.bookingPatron &&
-                        store.bookingPatron.cardnumber
-                      ? store.bookingPatron.cardnumber
-                      : newBooking.patron && newBooking.patron.cardnumber
-                        ? newBooking.patron.cardnumber
-                        : "",
+            content: renderContent(store.bookingPatron),
             type: "range",
             group: newBooking.item_id ? newBooking.item_id : 0,
         };
@@ -49,7 +50,7 @@ export function updateExternalDependents(store, newBooking, isUpdate = false) {
     }
     try {
         const countEls = document.querySelectorAll(".bookings_count");
-        countEls.forEach(el => {
+        countEls.forEach((el) => {
             let html = el.innerHTML;
             let match = html.match(/(\d+)/);
             if (match) {
