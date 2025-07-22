b1932a8f8c45bf4516fc4d08afd25ab0378df2b5
Paul Derscheid <paul.derscheid@lmscloud.de>
2025-07-22 10:58:19 +0200

    Local(fix): resolve incompatibilities of rspack, webpack configs, vue versions

--- rspack.config.js
+++ rspack.config.js
@@ -5,129 +5,118 @@ const rspack = require("@rspack/core");
 
 // Helper to create base config
 const createBaseConfig = (isOpac = false) => ({
-  resolve: {
-    alias: {
-      "@fetch": path.resolve(
-        __dirname,
-        "koha-tmpl/intranet-tmpl/prog/js/fetch"
-      ),
-      "@bookingApi": path.resolve(
-        __dirname,
-        isOpac
-          ? "koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingApi.opac.js"
-          : "koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingApi.js"
-      ),
+    resolve: {
+        alias: {
+            // Use Vue 3.5 for islands
+            "vue": path.resolve(__dirname, "node_modules/vue-3.5"),
+            "@fetch": path.resolve(
+                __dirname,
+                "koha-tmpl/intranet-tmpl/prog/js/fetch"
+            ),
+            "@bookingApi": path.resolve(
+                __dirname,
+                isOpac
+                    ? "koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingApi.opac.js"
+                    : "koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingApi.js"
+            ),
+        },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-        options: {
-          experimentalInlineMatchResource: true,
-        },
-        exclude: [path.resolve(__dirname, "t/cypress/")],
-      },
-      {
-        test: /\.ts$/,
-        loader: "builtin:swc-loader",
-        options: {
-          jsc: {
-            parser: {
-              syntax: "typescript",
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                options: {
+                    experimentalInlineMatchResource: true,
+                },
+                exclude: [path.resolve(__dirname, "t/cypress/")],
             },
-          },
-        },
-        exclude: [/node_modules/, path.resolve(__dirname, "t/cypress/")],
-        type: "javascript/auto",
-      },
-      {
-        test: /\.css$/i,
-        type: "javascript/auto",
-        use: ["style-loader", "css-loader"],
-      },
+            {
+                test: /\.ts$/,
+                loader: "builtin:swc-loader",
+                options: {
+                    jsc: {
+                        parser: {
+                            syntax: "typescript",
+                        },
+                    },
+                },
+                exclude: [
+                    /node_modules/,
+                    path.resolve(__dirname, "t/cypress/"),
+                ],
+                type: "javascript/auto",
+            },
+            {
+                test: /\.css$/i,
+                type: "javascript/auto",
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new rspack.DefinePlugin({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+            // Add environment variable for API module selection
+            __IS_OPAC__: isOpac,
+        }),
     ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new rspack.DefinePlugin({
-      __VUE_OPTIONS_API__: true,
-      __VUE_PROD_DEVTOOLS__: false,
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-      // Add environment variable for API module selection
-      __IS_OPAC__: isOpac,
-    }),
-  ],
-  externals: {
-    jquery: "jQuery",
-    "datatables.net": "DataTable",
-    "datatables.net-buttons": "DataTable",
-    "datatables.net-buttons/js/buttons.html5": "DataTable",
-    "datatables.net-buttons/js/buttons.print": "DataTable",
-    "datatables.net-buttons/js/buttons.colVis": "DataTable",
-  },
+    externals: {
+        jquery: "jQuery",
+        "datatables.net": "DataTable",
+        "datatables.net-buttons": "DataTable",
+        "datatables.net-buttons/js/buttons.html5": "DataTable",
+        "datatables.net-buttons/js/buttons.print": "DataTable",
+        "datatables.net-buttons/js/buttons.colVis": "DataTable",
+    },
 });
 
 module.exports = [
-  // Staff interface config
-  {
-    ...createBaseConfig(false),
-    entry: {
-      erm: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/erm.ts",
+    // Staff interface ESM config
+    {
+        ...createBaseConfig(false),
+        experiments: {
+            outputModule: true,
+        },
+        entry: {
+            islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
+        },
+        output: {
+            filename: "[name].esm.js",
+            path: path.resolve(
+                __dirname,
+                "koha-tmpl/intranet-tmpl/prog/js/vue/dist/"
+            ),
+            chunkFilename: "[name].[contenthash].esm.js",
+            globalObject: "window",
+            library: {
+                type: "module",
+            },
+        },
     },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(
-        __dirname,
-        "koha-tmpl/intranet-tmpl/prog/js/vue/dist/"
-      ),
-      chunkFilename: "[name].[contenthash].js",
-      globalObject: "window",
+    // OPAC interface config
+    {
+        ...createBaseConfig(true),
+        experiments: {
+            outputModule: true,
+        },
+        entry: {
+            islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
+        },
+        output: {
+            filename: "[name].esm.js",
+            path: path.resolve(
+                __dirname,
+                "koha-tmpl/opac-tmpl/bootstrap/js/vue/dist/"
+            ),
+            chunkFilename: "[name].[contenthash].esm.js",
+            globalObject: "window",
+            library: {
+                type: "module",
+            },
+        },
     },
-  },
-  // Staff interface ESM config
-  {
-    ...createBaseConfig(false),
-    experiments: {
-      outputModule: true,
-    },
-    entry: {
-      islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
-    },
-    output: {
-      filename: "[name].esm.js",
-      path: path.resolve(
-        __dirname,
-        "koha-tmpl/intranet-tmpl/prog/js/vue/dist/"
-      ),
-      chunkFilename: "[name].[contenthash].esm.js",
-      globalObject: "window",
-      library: {
-        type: "module",
-      },
-    },
-  },
-  // OPAC interface config
-  {
-    ...createBaseConfig(true),
-    experiments: {
-      outputModule: true,
-    },
-    entry: {
-      islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
-    },
-    output: {
-      filename: "[name].esm.js",
-      path: path.resolve(
-        __dirname,
-        "koha-tmpl/opac-tmpl/bootstrap/js/vue/dist/"
-      ),
-      chunkFilename: "[name].[contenthash].esm.js",
-      globalObject: "window",
-      library: {
-        type: "module",
-      },
-    },
-  },
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,24 @@ const { VueLoaderPlugin } = require("vue-loader");
 const path = require("path");
 const webpack = require("webpack");
 
-const createBaseConfig = (isOpac = false) => ({
+module.exports = {
+    entry: {
+        erm: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/erm.ts",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(
+            __dirname,
+            "koha-tmpl/intranet-tmpl/prog/js/vue/dist/"
+        ),
+        chunkFilename: "[name].js",
+    },
     resolve: {
         alias: {
-            "@fetch": path.resolve(
-                __dirname,
-                "koha-tmpl/intranet-tmpl/prog/js/fetch"
-            ),
-            "@bookingApi": path.resolve(
-                __dirname,
-                isOpac
-                    ? "koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingApi.opac.js"
-                    : "koha-tmpl/intranet-tmpl/prog/js/modals/place_booking/bookingApi.js"
-            ),
+            // Prevent webpack from trying to compile islands.ts
+            "./islands": false,
+            "./islands.ts": false,
         },
-        extensions: [".ts", ".js", ".vue", ".json"], 
     },
     module: {
         rules: [
@@ -30,14 +33,15 @@ const createBaseConfig = (isOpac = false) => ({
                 loader: "ts-loader",
                 options: {
                     appendTsSuffixTo: [/\.vue$/],
+                    transpileOnly: true,
                 },
                 exclude: [
-                    /node_modules/,
                     path.resolve(__dirname, "t/cypress/"),
+                    /islands\.ts$/,
                 ],
             },
             {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: ["style-loader", "css-loader"],
             },
         ],
@@ -48,84 +52,9 @@ const createBaseConfig = (isOpac = false) => ({
             __VUE_OPTIONS_API__: true,
             __VUE_PROD_DEVTOOLS__: false,
             __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-            __IS_OPAC__: isOpac,
+        }),
+        new webpack.IgnorePlugin({
+            resourceRegExp: /islands\.ts$/,
         }),
     ],
-    externals: {
-        jquery: "jQuery",
-        "datatables.net": "DataTable",
-        "datatables.net-buttons": "DataTable",
-        "datatables.net-buttons/js/buttons.html5": "DataTable",
-        "datatables.net-buttons/js/buttons.print": "DataTable",
-        "datatables.net-buttons/js/buttons.colVis": "DataTable",
-    },
-});
-
-module.exports = [
-    {
-        ...createBaseConfig(false),
-        entry: {
-            erm: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/erm.ts",
-            preservation:
-                "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/preservation.ts",
-            "admin/record_sources":
-                "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/admin/record_sources.ts",
-            acquisitions:
-                "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/acquisitions.ts",
-        },
-        output: {
-            filename: "[name].js",
-            path: path.resolve(
-                __dirname,
-                "koha-tmpl/intranet-tmpl/prog/js/vue/dist/"
-            ),
-            chunkFilename: "[name].[contenthash].js",
-            globalObject: "window",
-        },
-        mode: "development",
-    },
-    {
-        ...createBaseConfig(false),
-        experiments: {
-            outputModule: true,
-        },
-        entry: {
-            islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
-        },
-        output: {
-            filename: "[name].esm.js",
-            path: path.resolve(
-                __dirname,
-                "koha-tmpl/intranet-tmpl/prog/js/vue/dist/"
-            ),
-            chunkFilename: "[name].[contenthash].esm.js",
-            globalObject: "window",
-            library: {
-                type: "module",
-            },
-        },
-        mode: "development",
-    },
-    {
-        ...createBaseConfig(true),
-        experiments: {
-            outputModule: true,
-        },
-        entry: {
-            islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
-        },
-        output: {
-            filename: "[name].esm.js",
-            path: path.resolve(
-                __dirname,
-                "koha-tmpl/opac-tmpl/bootstrap/js/vue/dist/"
-            ),
-            chunkFilename: "[name].[contenthash].esm.js",
-            globalObject: "window",
-            library: {
-                type: "module",
-            },
-        },
-        mode: "development",
-    },
-];
+};
