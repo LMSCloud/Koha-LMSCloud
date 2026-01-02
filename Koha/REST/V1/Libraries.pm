50b4c527dac1463ee3c66133a4163a308983c318
Paul Derscheid <paul.derscheid@lmscloud.de>
2026-01-02 15:53:15 +0100

    Local(feat): add holidays API endpoint for libraries
    - Add list_holidays controller to Libraries.pm using Koha::Calendar
    - Add OpenAPI spec for /libraries/{library_id}/holidays endpoint
    - Add public endpoint /public/libraries/{library_id}/holidays for OPAC
    - Returns array of closed day dates (YYYY-MM-DD) within a date range

--- Koha/REST/V1/Libraries.pm
+++ Koha/REST/V1/Libraries.pm
@@ -18,10 +18,12 @@ package Koha::REST::V1::Libraries;
 use Modern::Perl;
 
 use Mojo::Base 'Mojolicious::Controller';
+
+use DateTime;
+use Koha::Calendar;
+use Koha::DateUtils qw( dt_from_string );
 use Koha::Libraries;
-
 use Scalar::Util qw( blessed );
-
 use Try::Tiny qw( catch try );
 
 =head1 NAME
@@ -163,6 +165,61 @@ sub delete {
     return try {
         $library->delete;
         return $c->render( status => 204, openapi => '');
+    }
+    catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 list_holidays
+
+Controller function that returns closed days for a library within a date range.
+Used by booking calendar to disable selection of closed days.
+
+=cut
+
+sub list_holidays {
+    my $c = shift->openapi->valid_input or return;
+
+    my $library_id = $c->param('library_id');
+    my $from       = $c->param('from');
+    my $to         = $c->param('to');
+
+    my $library = Koha::Libraries->find($library_id);
+
+    if ( !$library ) {
+        return $c->render(
+            status  => 404,
+            openapi => { error => 'Library not found' }
+        );
+    }
+
+    return try {
+        my $from_dt = $from ? dt_from_string( $from, 'iso' ) : dt_from_string();
+        my $to_dt   = $to   ? dt_from_string( $to,   'iso' ) : $from_dt->clone->add( months => 3 );
+
+        if ( $to_dt->compare($from_dt) < 0 ) {
+            return $c->render(
+                status  => 400,
+                openapi => { error => "'to' date must be after 'from' date" }
+            );
+        }
+
+        my $calendar = Koha::Calendar->new( branchcode => $library_id );
+        my $holidays = [];
+
+        my $current = $from_dt->clone;
+        while ( $current <= $to_dt ) {
+            if ( $calendar->is_holiday($current) ) {
+                push @{$holidays}, $current->ymd;
+            }
+            $current->add( days => 1 );
+        }
+
+        return $c->render(
+            status  => 200,
+            openapi => $holidays
+        );
     }
     catch {
         $c->unhandled_exception($_);
