caa017ff05e87ca0ff8815e15f04dd414a627cbc
roger <roger@kallarm.fritz.box>
2026-04-07 14:09:37 +0200

    Fix a problem with parameter prefix which could be misused.
    (cherry picked from commit a5f3539414745048f64447d8736b83b800c28785)

--- opac/opac-browser-sfb.pl
+++ opac/opac-browser-sfb.pl
@@ -126,11 +126,9 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
 # the level of browser to display
 my $level  = $query->param('level') || 0;
 my $filter = $query->param('filter');
-my $prefix = $query->param('prefixed');
 my $scrubber = C4::Scrubber->new();
 $level = $scrubber->scrub($level) if ( $level );
 $filter = $scrubber->scrub($filter) if ( $filter );
-$prefix = $scrubber->scrub($prefix) if ( $prefix );
 
 my ( $countEntries, $countFolders, $youthcount, $adultcount, $childcount, $musiccount, $levelEntries ) =
     ( 0, 0, 0, 0, 0, 0, 0 );
@@ -272,8 +270,7 @@ $template->param(
     LOOP_COUNT         => scalar(@level_loop),
     LEVEL              => $level,
     have_hierarchy     => $have_hierarchy,
-    MYENTRY            => $myentry,
-    PREFIXED           => $prefix
+    MYENTRY            => $myentry
 );
 
 sub createSearchString {
