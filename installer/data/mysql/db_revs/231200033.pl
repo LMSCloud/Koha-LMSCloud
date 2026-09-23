cc7edcfd099d478acbb4e52cf75ad8d2be65640c
Paul Derscheid <paul.derscheid@lmscloud.de>
2026-09-23 10:55:52 +0200

    Local(fix): Keep the LMS search_field.type members in db_rev 231200033
    - Carry string_plus and availability through the geo_point enum change,
      matching kohastructure.sql and the LMS versions of 211200032 and
      220600017
    - Restore NOT NULL and the column comment, which the community ALTER
      dropped
    - Without this, upgrading from 22.11 truncates LMS-typed search fields to
      '' in non-strict SQL mode and aborts the upgrade in strict mode

--- installer/data/mysql/db_revs/231200033.pl
+++ installer/data/mysql/db_revs/231200033.pl
@@ -8,7 +8,11 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         $dbh->do(
-            q{ alter table search_field MODIFY COLUMN type enum('','string','date','number','boolean','sum','isbn','stdno','year','callnumber','geo_point') }
+            q{
+            ALTER TABLE `search_field` MODIFY COLUMN `type`
+            enum('','string','date','number','boolean','sum','isbn','stdno','year','callnumber','geo_point','string_plus','availability') NOT NULL
+            COMMENT 'what type of data this holds, relevant when storing it in the search engine'
+        }
         );
         say $out "Added new value 'geo_point' to search_field.type enum";
     },
